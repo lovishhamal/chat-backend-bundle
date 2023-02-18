ec14ee7df937515ed724850bce67bb84d1f5b893
Lovish Hamal <lovishhamal@Lovishs-Laptop.local>
2023-02-18 21:13:20 +0545

    fixed initiate call issue

--- dist/enums/common.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ConnectionType = void 0;
-var ConnectionType;
-(function (ConnectionType) {
-    ConnectionType["INDIVIDUAL"] = "individual";
-    ConnectionType["GROUP"] = "group";
-})(ConnectionType = exports.ConnectionType || (exports.ConnectionType = {}));
-//# sourceMappingURL=common.js.map
